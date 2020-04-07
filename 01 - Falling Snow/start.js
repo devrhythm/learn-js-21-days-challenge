74b15cc392d26760b5c7620931a86c600275f9e0
SUMETH KONGPRASERT <sumeth@ibss.co.th>
2020-04-07 20:56:58 +0700

    day 1: set up canvas

--- 01 - Falling Snow/start.js
+++ 01 - Falling Snow/start.js
@@ -1,5 +1,20 @@
 (() => {
     // เริ่มเขียนโค้ด
-  
-  })();
-  
+  function setup() {
+    const canvas = document.getElementById('falling-snow-canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    return {
+      canvas,
+      canvasContext: canvas.getContext('2d'),
+      numberOfSnowBall: 250
+    }
+  }
+
+  function run() {
+    const {canvas, canvasContext, numberOfSnowBall} = setup();
+  }
+
+  run();
+})();
